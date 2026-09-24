12dbd8c8c9d5cb60ad83dada6cb7bca170c9d224
earthtraveller1 <liangyuezhao7@gmail.com>
2026-09-24 13:53:03 -0400

    Disabled the dimming in Vim which causes strange opaqueness to occur.

--- nvim/.config/nvim/lua/earthtraveller1/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/earthtraveller1/plugins/colorscheme.lua
@@ -3,12 +3,12 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-        transparent_background = false,
-        dim_inactive = {
-            enabled = true,
-            shade = "dark",
-            percentage = 0.15,
-        }
+        transparent_background = true,
+        -- dim_inactive = {
+        --     enabled = true,
+        --     shade = "dark",
+        --     percentage = 0.15,
+        -- }
     },
     config = function(_, opts)
         require("catppuccin").setup(opts)
