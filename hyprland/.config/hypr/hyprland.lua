13156e4c952d5e91e474d089af8c6ceb016bc35a
earthtraveller1 <liangyuezhao7@gmail.com>
2026-09-25 13:31:19 -0400

    The cursor sizes seems to be confusing for the screenshot utilities

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -74,9 +74,6 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-
 -- For XWayland applications
 hl.env("GDK_SCALE", local_settings.scale)
 hl.env("XCURSOR_SIZE", 16 * local_settings.scale)
